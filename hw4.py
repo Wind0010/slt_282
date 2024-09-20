b4c325385f5dc911679b36080e7be67c537b025d
Wind0010 <redminote10azg@gmail.com>
2024-09-20 19:08:30 +0600

    hw4.py with 3 codes

---
+++ hw4.py
@@ -0,0 +1,42 @@
+class AdvancedBank:
+    pass
+
+
+
+bank_instance = AdvancedBank("My Bank", 1000, 5000)
+bank_instance.interest_rate = 0.07  
+bank_instance.credit_limit = 6000
+
+
+
+
+
+
+
+
+import turtle
+
+t = turtle.Turtle()
+for i in range(36):
+    t.forward(100)
+    t.right(170)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from original_module import Hello  
+
+class NewHello(Hello):
+    def new_method(self):
+
+        pass
