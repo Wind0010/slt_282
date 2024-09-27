2d66ab8563fafa00a0f1aabe987ced33ca4a5797
Wind0010 <redminote10azg@gmail.com>
2024-09-27 20:48:15 +0600

    new-decorator-feature.py

--- new-decorator-feature.py
+++ new-decorator-feature.py
@@ -1,1 +1,18 @@
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("wassup")
+        result = func(*args, **kwargs)
+        print("what did u need to be a comfortable.")
+        return result
+    return wrapper
 
+@my_decorator
+def say_hello(name):
+    print(f"Hello, {name}!")
+
+git add decorator_house.py
+git commit -m "Добавлен декоратор в decorator_house.py"
+git push -u origin new-decorator-feature
+
+
+say_hello("Peace")
