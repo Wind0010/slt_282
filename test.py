c1793667f527d51113f66ff650955316ed3cce4b
Wind0010 <redminote10azg@gmail.com>
2024-09-27 19:57:10 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#Question 1 Вопрос 1
 git clone https://github.com/Alb1ngid/Ftest.git
 cd Ftest
 
@@ -5,8 +6,7 @@ git remote remove origin
 git remote add origin https://github.comhttps://github.com/Alb1ngid/oop46/.git
 git push -u origin master
 
-
-
+#Question 2 Вопрос 2
 git checkout -b new-feature
 python -m venv venv
 venv\Scripts\activate
@@ -16,3 +16,11 @@ git rm reg.txt
 git add requirements.txt
 git commit -m "Создан requirements.txt, удалён reg.txt"
 git push -u origin new-feature
+
+#Question 3 Вопрос 3
+python "seed_phrase_generator (2).py"
+git checkout -b account-creation
+git add .
+git commit -m "Создание 50 аккаунтов"
+git push -u origin account-creation
+git checkout master
