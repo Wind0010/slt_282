dcd1df0ab43fc47e52dfb470368435b87f7e3dc5
Wind0010 <redminote10azg@gmail.com>
2024-09-27 19:45:10 +0600

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,15 @@ cd Ftest
 git remote remove origin
 git remote add origin https://github.comhttps://github.com/Alb1ngid/oop46/.git
 git push -u origin master
+
+
+
+git checkout -b new-feature
+python -m venv venv
+venv\Scripts\activate
+pip install -r reg.txt
+pip freeze > requirements.txt
+git rm reg.txt
+git add requirements.txt
+git commit -m "Создан requirements.txt, удалён reg.txt"
+git push -u origin new-feature
