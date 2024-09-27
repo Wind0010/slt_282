60980247f2663fe608ef5a561c4f77aa4fab33b9
Wind0010 <redminote10azg@gmail.com>
2024-09-27 19:36:35 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+git clone https://github.com/Alb1ngid/Ftest.git
+cd Ftest
 
+git remote remove origin
+git remote add origin https://github.comhttps://github.com/Alb1ngid/oop46/.git
+git push -u origin master
